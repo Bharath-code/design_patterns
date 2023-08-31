50027a3ab0564bd605333dc9e531f558ecfbdd3a
Bharath <kumarbharath63@gmail.com>
2023-08-31 17:55:37 +0530

    added comments

--- module_pattern/input.js
+++ module_pattern/input.js
@@ -1,3 +1,4 @@
+//validate the input type whether it's number or not
 export function validateInput(input){
     if(typeof input !== "number"){
        throw new Error("Invalid Input, kindly use number..");
